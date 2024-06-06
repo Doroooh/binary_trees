704c81e8bfa1d00ce0f52eea8db7c463d0d49060
Waithira <dorotttyyy@gmail.com>
2024-06-06 16:35:02 -0700

    binary trees

--- bnarytrees.h
+++ bnarytrees.h
@@ -2,19 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+/* Binary tree node structure */
 struct binary_tree_s
 {
 	int n;
@@ -26,21 +15,8 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-/*  linked list for advanced 101 the levelorder */
-/**
- * struct link_s - structure for advanced tasks
- *
- * @n: depth of node specified
- * @node: node of tree to store
- * @next: next node of the linked list
- */
-typedef struct link_s
-{
-	size_t n;
-	struct binary_tree_s const *node;
-	struct link_s *next;
-} link_t;
-/*  ----------------- mandatory task ---------------------------------*/
+
+/*the Function declarations */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -61,8 +37,9 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second);
+
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -82,3 +59,5 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+#endif /* _BINARY_TREES_H_ */
