f9dfe4caecf34f18b1c091de13f289ec7d633b2e
Waithira <dorotttyyy@gmail.com>
2024-06-06 18:15:51 -0700

    binary  trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - function that says if a tree is full or not
- * a tree is full if it has tow or none children
- * @tree: tree to check
- * Return: 1 if it is a full 0 if not
+ * binary_tree_is_full - functions to check if a tree is full or not
+ * the tree is said to be  full if there are two or more children
+ * @tree: checking tree
+ * Return: 1 when full 0 when not
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
