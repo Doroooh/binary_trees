b49ae6e2e7024d1f37f38c3d4f4c2064e9ec7266
Waithira <dorotttyyy@gmail.com>
2024-06-06 18:27:17 -0700

    Binary trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,17 +2,17 @@
 
 /**
  * binary_tree_sibling - finding if the node is a sibling
- * @ndes: node checking
+ * @node: node checkingi
  * Return: sibling node
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (ndes == NULL || ndes->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-	if (ndes->parent->left == ndes)
-		return (ndes->parent->right);
-	return (ndes->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
