9ca140aac5b73d7f8f8baace81d875e5c5a4a110
Waithira <dorotttyyy@gmail.com>
2024-06-06 18:29:58 -0700

    binary trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,26 +6,20 @@
  * Return: uncle node
  */
 
-binary_tree_ *binary_tree_uncle(binary_tree_t *node)
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    // Check if the node, its parent, or its grandparent is NULL
     if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
     {
         return NULL;
     }
 
-    // Determine the parent node
-    binary_tree_t *parent = node->parent;
-    // Determine the grandparent node
-    binary_tree_t *grandparent = parent->parent;
-
-    // Check and return the uncle node
-    if (grandparent->left == parent)
+    if (node->parent == node->parent->parent->left)
     {
-        return grandparent->right;
+        return node->parent->parent->right;
     }
     else
     {
-        return grandparent->left;
+        return node->parent->parent->left;
     }
 }
